4b7b5293ad979df737dd29ec395cb8dae43055a6
Boskolife <94291571+Boskolife@users.noreply.github.com>
2024-10-04 17:19:36 +0300

    add pages

--- src/js/main.js
+++ src/js/main.js
@@ -10,3 +10,45 @@ function burgerInit() {
     document.body.classList.toggle('body_lock');
   });
 }
+
+const swiper = new Swiper('.swiper_certificate', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1.3,
+      spaceBetween: 15,
+      centeredSlides: true,
+      initialSlide: 1,
+    },
+    375: {
+      slidesPerView: 1.5,
+      spaceBetween: 25,
+      centeredSlides: true,
+      initialSlide: 1,
+    },
+    480: {
+      slidesPerView: 1.6,
+      spaceBetween: 25,
+      centeredSlides: true,
+      initialSlide: 1,
+    },
+    650: {
+      slidesPerView: 2,
+      spaceBetween: 25,
+      centeredSlides: true,
+      initialSlide: 1,
+    },
+    768: {
+      slidesPerView: 2.5,
+      spaceBetween: 25,
+      centeredSlides: true,
+      initialSlide: 1,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+});
